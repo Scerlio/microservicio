9cc89fc0c794a5161353dc73dd0f1d22ae5d3071
Ismael Alía <ismael.alia@soprabanking.com>
2019-05-16 08:41:10 +0200

    Eureka en gradle

--- ms-kafka-consumer/src/main/java/modelo/controladores/MsKafkaConsumerApplication.java
+++ ms-kafka-consumer/src/main/java/modelo/controladores/MsKafkaConsumerApplication.java
@@ -2,9 +2,11 @@ package modelo.controladores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableMongoRepositories("modelo.persistencia")
 @ComponentScan(basePackages= {"modelo", "config"})
--- ms-kafka-producer/src/main/java/cfg/MsKafkaProducerApplication.java
+++ ms-kafka-producer/src/main/java/cfg/MsKafkaProducerApplication.java
@@ -2,8 +2,10 @@ package cfg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages = "modelo")
 public class MsKafkaProducerApplication {
