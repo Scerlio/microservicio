b35c474fd95d953ad5703dc36c117aee34776dc9
Ismael Alía <ismael.alia@soprabanking.com>
2019-05-20 13:30:02 +0200

    Arreglado proxy

--- ms-kafka-producer/src/main/java/modelo/gestion/Recurso.java
+++ ms-kafka-producer/src/main/java/modelo/gestion/Recurso.java
@@ -26,11 +26,11 @@ public class Recurso {
 
 	private String TOPIC = "DOOM";	
 	private static KafkaConfig kc = new KafkaConfig();
-	
-	
+
+
 	// Se crea un logger para la clase
 	Logger log = LoggerFactory.getLogger(Recurso.class);
-	
+
 	@PostMapping(path="/mandarUsuario")
 	public ModelAndView mandarUsuario (@RequestParam("nombre") String nombre, 
 			@RequestParam("apellido") String apellido) throws InterruptedException, ExecutionException {
@@ -39,37 +39,37 @@ public class Recurso {
 		Usuario u = new Usuario();
 		u.setNombre(nombre);
 		u.setApellido(apellido);
-		
+
 		final Producer<String, Usuario> producer = kc.createProducer();
-		
+
 		ProducerRecord<String, Usuario> record = new ProducerRecord<String, Usuario>(TOPIC, u);
-		
+
 		// Operacion asincrona, añadimos un callback para recibir feedback del mensaje
 		producer.send(record, new Callback() {
-			
+
 			@Override
 			public void onCompletion(RecordMetadata metadata, Exception e) {
 				// Se ejecuta siempre que se manda o se genera una excepcion
 				if (e == null) {
 					log.info("\nMetadatos recibidos. Topic: " + metadata.topic() + " particion: "
-						+ metadata.partition() + " offset: " + metadata.offset() 
-						+ " key: " + record.key() + " time: " + metadata.timestamp());
+							+ metadata.partition() + " offset: " + metadata.offset() 
+							+ " key: " + record.key() + " time: " + metadata.timestamp());
 				}
-				
+
 				else {
 					log.error("Error", e);
 				}
 			}
 		});
-		
+
 		System.out.printf("sent record(key=%s value=(%s)) ", record.key(), record.value());
-		
+
 		// Se necesita para forzar el envio
 		producer.flush();
 		// Flush and close
 		producer.close();
-		
-		return new ModelAndView("redirect:http://localhost:8080/producer");
+
+		return new ModelAndView("redirect:producer");
 	}
-	
+
 }
